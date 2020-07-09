15ec96348f1a10e4897cb63e2b98cbc23659d4c4
Choi Yong Seok <dydtjr1994@gmail.com>
2020-07-09 17:36:53 +0900

    Update: add callback function, update example

--- FileWatcher/Main.cpp
+++ FileWatcher/Main.cpp
@@ -1,33 +1,95 @@
 ﻿#include <iostream>
+#include <chrono>
+#include <fstream>
 
 #include "FileWatcher.h"
 
+using namespace std::chrono_literals;
+
+void callback(std::filesystem::path path, watcher::FileStatus status) {
+	switch (status)
+	{
+	case watcher::FileStatus::Created:
+		std::cout << path.string() << " Created!" << std::endl;
+		break;
+	case watcher::FileStatus::Modified:
+		std::cout << path.string() << " Modified!" << std::endl;
+		break;
+	case watcher::FileStatus::Erased:
+		std::cout << path.string() << " Erased!" << std::endl;
+		break;
+	default:
+		std::cout << "error!" << std::endl;
+		break;
+	}
+}
+
+void eachCallback(std::filesystem::path path, watcher::FileStatus status) {
+	std::cout << "each callback ";
+	switch (status)
+	{
+	case watcher::FileStatus::Created:
+		std::cout << path.string() << " Created!" << std::endl;
+		break;
+	case watcher::FileStatus::Modified:
+		std::cout << path.string() << " Modified!" << std::endl;
+		break;
+	case watcher::FileStatus::Erased:
+		std::cout << path.string() << " Erased!" << std::endl;
+		break;
+	default:
+		std::cout << "error!" << std::endl;
+		break;
+	}
+}
+
+void writeData(std::filesystem::path path) {
+	std::filesystem::create_directories(path.parent_path());
+	std::ofstream ofs(path, std::ios::app);
+	ofs.close();
+
+	std::this_thread::sleep_for(5s);
+
+	std::ofstream ofs2(path, std::ios::app);
+	ofs2 << "this is some text in the new file\n";
+	ofs2.close();
+}
+
 int main() {
+
+
 	watcher::FileWatchar fileWatchar;
-	fileWatchar.addPath("C:/Users/CYS/Downloads/temp2.txt"); // Add path that want to chage(Created, Modified, Erased)
-	fileWatchar.addPath("C:/Users/CYS/Downloads/temp1.txt");
 
-	fileWatchar.start([](std::filesystem::path path, watcher::FileStatus status) { // Callback
-		switch (status)
-		{
-		case watcher::FileStatus::Created:
-			std::cout << path.string() << " Created!" << std::endl;
-			break;
-		case watcher::FileStatus::Modified:
-			std::cout << path.string() << " Modified!" << std::endl;
-			break;
-		case watcher::FileStatus::Erased:
-			std::cout << path.string() << " Erased!" << std::endl;
-			break;
-		default:
-			std::cout << "error!" << std::endl;
-			break;
-		}
-		});
-	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-	fileWatchar.addPath("C:/Users/CYS/Downloads/temp.txt");
-	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-	fileWatchar.removePath("C:/Users/CYS/Downloads/temp.txt");
+	std::filesystem::path temp("C:/Users/CYS/Downloads/temp.txt"), temp1("C:/Users/CYS/Downloads/temp1.txt"), temp2("C:/Users/CYS/Downloads/temp2.txt");
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(10000000)); // Do something
+	std::filesystem::remove_all(temp);
+	std::filesystem::remove_all(temp1);
+	std::filesystem::remove_all(temp2);	
+
+	// EX) Create file & write data
+	writeData(temp1);
+
+	// Add path that want to chage(Created, Modified, Erased)
+	// Support watching that not created file.
+	fileWatchar.addPath(temp1);
+
+	// Support each callback
+	fileWatchar.addPath(temp2, eachCallback);
+	fileWatchar.addPath("C:/Users/CYS/Downloads/temp3.txt", eachCallback); // It's ok to write string
+
+	fileWatchar.start(callback); // Global Callback
+
+	// EX) Create file & write data
+	writeData(temp2);
+
+	std::this_thread::sleep_for(5s);
+	fileWatchar.addPath(temp);
+	writeData(temp);
+	std::this_thread::sleep_for(5s);
+	fileWatchar.removePath(temp);
+	writeData(temp); // not write
+
+	//fileWatchar.stop(); // not essential
+
+	std::this_thread::sleep_for(1000s); // Do something
 }
